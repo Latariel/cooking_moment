a530e32b1a44c905d4b54b049cc4e6b8e69c339f
conterfeit <selenian@yandex.ru>
2024-05-22 14:19:14 +0300

    third_page_2

--- src/modal/modal_page_3/Modal_3_5.jsx
+++ src/modal/modal_page_3/Modal_3_5.jsx
@@ -5,12 +5,18 @@ import './Modal_3_5.css'
 import { IoCloseSharp } from "react-icons/io5";
 
 
-import rec1_1 from '../img_3/recept_5_1.jpg';
-import rec1_2 from '../img_3/recept_5_2.jpg';
-import rec1_3 from '../img_3/recept_5_3.jpg';
-import rec1_4 from '../img_3/recept_5_4.jpg';
-import rec1_5 from '../img_3/recept_5_5.jpg';
-import rec1_6 from '../img_3/recept_5_6.jpg';
+import rec1_1 from '../img_4/recept_5_1.jpg';
+import rec1_2 from '../img_4/recept_5_2.jpg';
+import rec1_3 from '../img_4/recept_5_3.jpg';
+import rec1_4 from '../img_4/recept_5_4.jpg';
+import rec1_5 from '../img_4/recept_5_5.jpg';
+import rec1_6 from '../img_4/recept_5_6.jpg';
+import rec1_7 from '../img_4/recept_5_7.jpg';
+import rec1_8 from '../img_4/recept_5_8.jpg';
+import rec1_9 from '../img_4/recept_5_9.jpg';
+import rec1_10 from '../img_4/recept_5_10.jpg';
+import rec1_11 from '../img_4/recept_5_11.jpg';
+import rec1_12 from '../img_4/recept_5_12.jpg';
 import star from "../img_2/Shape.png";
 
 
@@ -37,13 +43,13 @@ export const Modal_3_5 = ({ isOpen, onClose, children }) => {
             <h2 className={'text_modal_rec_2'}>
                 Ингредиенты: <br/>
                 <br/>
-                Перец сладкий - 8 шт. <br/>
-                Фарш свино-говяжий - 400 г<br/>
+                Картофель - 1,4 кг <br/>
+                Фарш - 400 г<br/>
                 Чеснок (1-2 зубчика)<br/>
-                Морковь (1-2 шт)<br/>
-                Рис круглозёрный 1/3 стакана<br/>
-                Томатная паста – 3 ст.л<br/>
-                Сметана - 150 г<br/>
+                Лук (1-2 шт)<br/>
+                Масло сливочное 50 г<br/>
+                Яйца 1 шт.<br/>
+                Сыр - 100 г <br/>
 
 
 
@@ -54,43 +60,85 @@ export const Modal_3_5 = ({ isOpen, onClose, children }) => {
 
                         <img className={'img_rec'} src={rec1_1}/>
                         <h3 className={'txt1_1'}>
-                            Лук нарежьте кубиками, морковь измельчите на тёрке</h3>
+                            Очищаем картофель, нарезаем его на кусочки и ставим его вариться до готовности</h3>
                     </div>
                     <div className="grid-5">
 
                         <img className={'img_rec'} src={rec1_2}/>
-                        <h3 className={'txt2_2'}>Рис промойте, смешайте с фаршем и овощами, посолите, поперчите.</h3>
+                        <h3 className={'txt3'}>Очищаем лук  и мелко его нарезаем.</h3>
                     </div>
                     <div className="grid-5">
 
                         <img className={'img_rec'} src={rec1_3}/>
-                        <h3 className={'txt1_1'}>Вмешайте в начинку примерно 100 мл воды.</h3>
+                        <h3 className={'txt1_1'}>разогреваем сковороду с растительным маслом и обжариваем лук до золотистого состояния.</h3>
 
                     </div>
                     <div className="grid-5">
 
                         <img className={'img_rec'} src={rec1_4}/>
-                        <h3 className={'txt2_2'}>У перцев аккуратно срежьте верхнюю часть и удалите семечки. Нафаршируйте перцы начинкой не слишком плотно.</h3>
+                        <h3 className={'txt1'}>Добавляем в сковороду фарш (500 г), примерно 1/4 чайной ложки соли и чеснок (3-4 зубчика)</h3>
 
                     </div>
                     <div className="grid-5">
 
 
                         <img className={'img_rec'} src={rec1_5}/>
-                        <h3 className={'txt2_2'}>Сложите перцы в кастрюлю, влейте стакан воды, доведите до кипения и убавьте огонь. Добавьте в кастрюлю сметану и томатную пасту, посолите.</h3>
+                        <h3 className={'txt2_2'}>Разминаем картофель и делаем картофельное пюре</h3>
 
                     </div>
                     <div className="grid-5">
 
 
                         <img className={'img_rec'} src={rec1_6}/>
-                        <h3 className={'txt2_2'}>Накройте кастрюлю крышкой и тушите 40-45 минут до готовности.</h3>
+                        <h3 className={'txt4'}>Сыр (100 г) натираем на крупной тёрке.</h3>
+
+                    </div>
+                    <div className="grid-5">
+
+
+                        <img className={'img_rec'} src={rec1_7}/>
+                        <h3 className={'txt2'}>Собирать будем по-классике: картофель-фарш-картофель-сыр.</h3>
+
+                    </div>
+                    <div className="grid-5">
+
+
+                        <img className={'img_rec'} src={rec1_8}/>
+                        <h3 className={'txt2_2'}>Первый нижний слой — картофельное пюре.</h3>
+
+                    </div>
+                    <div className="grid-5">
+
+
+                        <img className={'img_rec'} src={rec1_9}/>
+                        <h3 className={'txt2_2'}>Далее выкладываем фарш, также равномерно его распределяем по всей площади формы и выравниваем.</h3>
+
+                    </div>
+                    <div className="grid-5">
+
+
+                        <img className={'img_rec'} src={rec1_10}/>
+                        <h3 className={'txt2_2'}>Теперь аккуратно выкладываем оставшееся картофельное пюре</h3>
+
+                    </div>
+                    <div className="grid-5">
+
+
+                        <img className={'img_rec'} src={rec1_11}/>
+                        <h3 className={'txt2'}>Равномерно посыпаем сверху тёртым сыром. </h3>
+
+                    </div>
+                    <div className="grid-5">
+
+
+                        <img className={'img_rec'} src={rec1_12}/>
+                        <h3 className={'txt2_2'}>Разогреваем духовку до 200°С и ставим в неё форму на 15-20 минут.</h3>
 
                     </div>
                 </div>
             </div>
 
-            <img className={'star_8'} src={star}/>
+            <img className={'star_9'} src={star}/>
         </div>
     );
 
